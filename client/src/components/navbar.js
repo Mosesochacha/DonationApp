c2f47cfef203791256e5281c53c8887b868c2ddb
Wahome <vinwahome@gmail.com>
2023-08-01 11:11:32 +0300

    Add the logo

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import logo1 from "../path.png";
+import logo from "./path.png";
 
 function Navbar(){
     return(
@@ -8,7 +8,6 @@ function Navbar(){
             <nav class="navbar navbar-expand-md sticky-navbar ">
       <div class="container">
         <a class="navbar-brand d-md-none d-xs-block py-3" href="#">
-          {/* <img src={logo1} height="40" alt="Company Logo"/> */}
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -16,6 +15,10 @@ function Navbar(){
     
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav mx-auto">
+            <li >
+                <img src={logo} className="nav-logo" alt="Company Logo"/>
+                 </li>
+
             <li class="nav-item">
               <Link to='/' class="nav-link mx-2" active aria-current="page" >Home</Link>
             </li>
@@ -29,8 +32,8 @@ function Navbar(){
             <li class="nav-item">
               <Link to='/aboutus' class="nav-link mx-2">About Us</Link>
             </li>
-            <li>
-              {/* <button type="button" class="btn btn-primary" id="donate-btn" >Donate</button> */}
+            <li  class="nav-item">
+              
               <Link to='/donations' class=" btn btn-primary" id="donate-btn" >Donate</Link>
             </li>
             
